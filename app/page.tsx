4a585d87c0e3f421e8969da8ba26b5fd3d92ea10
RG <101654057+jrguill3n@users.noreply.github.com>
2026-08-12 14:47:26 -0500

    Hide Instagram feed carousel

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { SiteHeader } from '@/components/site-header'
 import { SiteFooter } from '@/components/site-footer'
 import { Hero } from '@/components/home/hero'
 import { Intro } from '@/components/home/intro'
-import { InstagramFeed } from '@/components/home/instagram-feed'
 
 export default function HomePage() {
   return (
@@ -11,7 +10,6 @@ export default function HomePage() {
       <main>
         <Hero />
         <Intro />
-        <InstagramFeed />
       </main>
       <SiteFooter />
     </>
